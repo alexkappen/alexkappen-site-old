b28f7acd11dafa8dbec313e452ded9f98adab8fa
Alex Kappen <alexkappen@gmail.com>
2020-12-24 13:12:03 -0500

    canvas scrolls into view on btn click

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,5 +2,6 @@ const skillDetails = document.querySelector('#skills-details');
 
 const canvasRevealBtn = document.querySelector('#reveal-canvas').addEventListener('click', () => {
   skillDetails.classList.toggle('hide');
+  skillDetails.scrollIntoView();
 });
 
