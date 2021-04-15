b9038c256544876217a656d845352e8d5fa7ca40
Alex Kappen <alexkappen@gmail.com>
2021-04-14 22:05:05 -0400

    Fixed bayes search explanation logic

--- assets/scripts/bayes/main.js
+++ assets/scripts/bayes/main.js
@@ -50,7 +50,7 @@ generateTableHead(table, searchKeys);
 let item = document.querySelector(".itemInput").value;
 
 let randomItems = ["watch", "alarm clock", "Kermit the Frog muppet", "Nintendo DS",
-"keys", "Swiss Army knife"];
+"water bottle", "Swiss Army knife", "shirt"];
 
 if (item === null || item === "no" || item === "") {
   const randomIndex = Math.floor(Math.random() * randomItems.length);
